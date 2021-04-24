320e5eee0fa6700135b2b4a020d9291115ec833f
erafanomezan <eunnella@gmail.com>
2021-04-24 20:25:15 +0200

    modification des opérateurs

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeNOT.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeNOT.java
@@ -2,11 +2,13 @@ package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.BINOP;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.Exp;
+import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
 
 public class StreeNOT extends Stree {
 
 	private Exp exp;
+	private Type type;
 
 	public StreeNOT(Stree left) throws StreeException, TypeException {
 		super(left);
@@ -16,6 +18,22 @@ public class StreeNOT extends Stree {
 	@Override
 	public Exp getExp(){
 		return exp;
+	}
+
+	@Override
+	public Type getType() throws StreeException {
+		return type;
+	}
+
+	@Override
+	public boolean checkType() throws StreeException {
+		Type typeLeft = getLeft().getType();
+		
+		type = typeLeft;
+		if ((typeLeft != null))
+			return typeLeft.assertBoolean(); // pas sure de assertBoolean ici
+		else
+			throw new StreeException("Type error while checking null types !");
 	}
 
 }
